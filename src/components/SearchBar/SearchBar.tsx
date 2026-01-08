05a6d2b337a95445b85c99114405259bc94f989c
SoftPillow20 <Nobila.Bernie19@gmail.com>
2026-01-08 22:27:53 +0800

    added static input

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -4,6 +4,18 @@ function SearchBar() {
   return (
     <div className={styles.searchBar}>
       <h1 className="title">How's the sky looking today?</h1>
+      <div>
+        <div className={styles.search}>
+          <input
+            name="searchBar"
+            type="text"
+            placeholder="Search for a place..."
+          />
+          <img src="./assets/images/icon-search.svg" alt="search icon" />
+        </div>
+        {/* This should be a link rather than a button. Reason is for user to be able to view previous request and current request at runtime. */}
+        {/* <Link to='' /> */}
+      </div>
     </div>
   );
 }
