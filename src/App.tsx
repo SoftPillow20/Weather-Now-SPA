f1c39a57f943d02c28143b53d4cf427c6790b656
SoftPillow20 <Nobila.Bernie19@gmail.com>
2026-01-31 16:09:32 +0800

    fetched weather data

--- src/App.tsx
+++ src/App.tsx
@@ -76,9 +76,57 @@ function App() {
 
   // Get the weather API data
   useEffect(() => {
-    async function getWeatherData() {}
+    async function getWeatherData() {
+      const params = {
+        latitude: selectedCity.latitude,
+        longitude: selectedCity.longitude,
+        current: [
+          "temperature_2m",
+          "relative_humidity_2m",
+          "apparent_temperature",
+          "is_day",
+          "precipitation",
+          "wind_speed_10m",
+        ],
+      };
+
+      console.log(selectedCity);
+
+      // Guard clause: checks if there's a selected city
+      if (Object.keys(selectedCity).length === 0) return;
+
+      const responses = await fetchWeatherApi(URL, params);
+
+      const response = responses[0];
+
+      const current = response.current()!;
+
+      // Note: The order of weather variables in the URL query and the indices below need to match!
+      const weatherData = {
+        current: {
+          time: new Date().toDateString(),
+          temperature_2m: Math.round(current.variables(0)!.value()),
+          relative_humidity_2m: current.variables(1)!.value(),
+          apparent_temperature: Math.round(current.variables(2)!.value()),
+          is_day: current.variables(3)!.value(),
+          precipitation: Math.round(current.variables(4)!.value()),
+          wind_speed_10m: Math.round(current.variables(5)!.value()),
+        },
+      };
+
+      // The 'weatherData' object now contains a simple structure, with arrays of datetimes and weather information
+      console.log(
+        `\nCurrent time: ${weatherData.current.time}\n`,
+        `\nCurrent temperature_2m: ${weatherData.current.temperature_2m}`,
+        `\nCurrent relative_humidity_2m: ${weatherData.current.relative_humidity_2m}`,
+        `\nCurrent apparent_temperature: ${weatherData.current.apparent_temperature}`,
+        `\nCurrent is_day: ${weatherData.current.is_day}`,
+        `\nCurrent precipitation: ${weatherData.current.precipitation}`,
+        `\nCurrent wind_speed_10m: ${weatherData.current.wind_speed_10m}`,
+      );
+    }
     getWeatherData();
-  }, []);
+  }, [selectedCity]);
 
   return (
     <div>
